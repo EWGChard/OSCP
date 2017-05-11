769db18840c3ddfd5ef17fb94b5e3c1b44de941f
EWGChard <richardevans0614@hotmail.co.uk>
2017-05-11 10:31:21 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo "DO SUMMAT HERE!"
+echo "Welcome to Chards Ping Sweeper!"  
+sleep 5s                                
+echo "Please enter the Network address you would like to sweep: \n"
+read "Network: " netSweep
+echo "Sweeping..."
+for x in 'seq 1 254'; do
+ping -c 1 netSweep.$x | grep "64 bytes" | cut -d " " -f 4 | sed 's/.$//'
+done
